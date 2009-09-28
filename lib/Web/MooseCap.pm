a82e627304dfbd9559cdfef575bd5fa13ca4ada7
Dave Doyle <ddoyle@oanda.com>
2009-09-28 17:56:16 -0400

    continuing move of Stash to role

--- lib/Web/MooseCap.pm
+++ lib/Web/MooseCap.pm
@@ -185,40 +185,6 @@ sub param {
     return scalar @_ == 2 ? $_[1] : undef;
 }
 
-
-###################
-# the stash
-has '_stash' => (
-    is          => 'rw',
-    isa         => 'HashRef',
-    default     => sub { +{} },
-);
-
-# stash is reset before cgiapp_prerun
-sub stash {
-    my $self = shift;
-    my @args = @_;
-    
-    # if they want the hashref
-    return $self->_stash() if scalar @_ == 0;
-
-    # if they want to fetch a particular key ...
-    return $self->_stash->{$_[0]} if scalar @_ == 1 && ref $_[0] ne 'HASH' ;
-   
-    $self->merge_into($self->_stash, @_ );
-
-    return;
-}
-
-# overide this to add certain elements into the stash
-# happens immediately after stash reset and before cgiapp_prerun
-# default feeds the CGI object in
-sub cgiapp_stash_init {
-    my $self = shift;
-    $self->stash( query => $self->query() );
-    return;
-}
-
 #######################
 # tt with stash integration
 
@@ -716,8 +682,10 @@ sub add_callback {
                          ? ( $self_or_class, ref $self_or_class )
                          : (undef, $self_or_class);
 
-        die "Unknown hook ($hook)"
-            unless exists $class->__class_callbacks()->{$hook};
+    # can't add a callback unless it exists as a class callback
+    # via new_hook or in the default set
+    die "Unknown hook ($hook)"
+        unless exists $class->__class_callbacks()->{$hook};
                          
     # if $self, add it only to this instance's callback list
     if ( $self ) {
--- lib/Web/MooseCap/Role/Stash.pm
+++ lib/Web/MooseCap/Role/Stash.pm
@@ -3,5 +3,39 @@ package Web::MooseCap::Role::Stash;
 use Moose::Role;
 
 
+###################
+# the stash
+has '_stash' => (
+    is          => 'rw',
+    isa         => 'HashRef',
+    default     => sub { +{} },
+    init_arg    => undef,
+);
+
+# stash is reset before cgiapp_prerun
+sub stash {
+    my $self = shift;
+    my @args = @_;
+    
+    # if they want the hashref
+    return $self->_stash() if scalar @_ == 0;
+
+    # if they want to fetch a particular key ...
+    return $self->_stash->{$_[0]} if scalar @_ == 1 && ref $_[0] ne 'HASH' ;
+   
+    $self->merge_into($self->_stash, @_ );
+
+    return;
+}
+
+# overide this to add certain elements into the stash
+# happens immediately after stash reset and before cgiapp_prerun
+# default feeds the CGI object in
+sub cgiapp_stash_init {
+    my $self = shift;
+    $self->stash( query => $self->query() );
+    return;
+}
+
 
 no Moose::Role; 1;
